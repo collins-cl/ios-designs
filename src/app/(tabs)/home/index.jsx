63f4133c554dc5da468bec44a7f9df37c2774bb0
CoLe <collinsolads@gmail.com>
2026-08-01 03:09:03 +0100

    themed text

--- src/app/(tabs)/home/index.jsx
+++ src/app/(tabs)/home/index.jsx
@@ -1,14 +1,13 @@
 import { Stack, useRouter } from "expo-router";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Pressable, ScrollView, StyleSheet, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import ThemedText from "../../../components/ui/ThemedText";
 import { useTheme } from "../../../theme/ThemeProvider";
 
 export default function Index() {
   const { colors } = useTheme();
   const router = useRouter();
   const { top } = useSafeAreaInsets();
-
-  console.log(top);
 
   return (
     <>
@@ -43,12 +42,37 @@ export default function Index() {
           backgroundColor: colors.background,
         }}
       >
-        <ScrollView style={{ paddingTop: top + 54 }}>
-          {Array.from({ length: 40 }).map((_, index) => (
-            <View key={index}>
-              <Text style={{ color: colors.text }}>Item {index + 1}</Text>
-            </View>
-          ))}
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          style={{
+            paddingTop: top + 54,
+            marginBottom: 10,
+            paddingHorizontal: 16,
+          }}
+        >
+          <View style={{ display: "flex", flexDirection: "column", gap: 16 }}>
+            {Array.from({ length: 40 }).map((_, index) => (
+              <View key={index} style={{ width: 200, height: 40 }}>
+                <Pressable
+                  style={{
+                    width: 200,
+                    height: 40,
+                    borderColor: colors.border,
+                    borderWidth: 1,
+                    borderRadius: 20,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                  onPress={() => console.log("hello", index + 1)}
+                >
+                  <ThemedText colorName="primary" type="default">
+                    Link to this
+                  </ThemedText>
+                </Pressable>
+              </View>
+            ))}
+          </View>
         </ScrollView>
       </View>
     </>
---
+++ src/components/ui/ThemedText.jsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text } from "react-native";
+import { useTheme } from "../../theme/ThemeProvider";
+
+export default function ThemedText({
+  style,
+  colorName = "primary",
+  type = "default",
+  ...rest
+}) {
+  const { colors } = useTheme();
+  const textColor = { color: colors[colorName] ?? colors.text };
+
+  return <Text style={[textColor, styles[type], style]} {...rest} />;
+}
+
+const styles = StyleSheet.create({
+  default: {
+    fontSize: 16,
+    fontFamily: "Inter",
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: "InterSemiBold",
+  },
+  semiBold: {
+    fontSize: 18,
+    fontFamily: "InterSemiBold",
+  },
+  bold: {
+    fontSize: 18,
+    fontFamily: "InterBold",
+  },
+  subtitle: {
+    fontSize: 16,
+    fontFamily: "Inter",
+  },
+  regular: {
+    fontSize: 14,
+    fontFamily: "InterRegular",
+  },
+});
